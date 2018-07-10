86983eccde4ccc993b9bbb0136c03b298b43ebba
erisdinh <erisdinh@gmail.com>
2018-07-10 12:57:49 +0700

    Conditions and Operators
    Lesson notes about Operations and Condition statements

---
+++ HelloWorld/Condition.py
@@ -0,0 +1,23 @@
+numOfStudent = 3
+
+if(numOfStudent > 5):
+    print("The class has more than 5 students")
+else:
+    print("The class has less than 5 students")
+
+print(5==2)
+print(5!=2)
+
+age = 16
+
+if (age < 13):
+    print("You are young")
+elif (age >= 13 and age < 18):
+    print("You are a teenager")
+else:
+    print("You are an adult")
+
+if (age < 13 or age > 18):
+    print("You are not in the program for teenagers")
+else:
+    print("Welcome to the Teenagers program")
