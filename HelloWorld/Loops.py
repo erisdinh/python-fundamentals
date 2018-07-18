7239aa5331a17501c4f89875c593b8702574b9b8
erisdinh <erisdinh@gmail.com>
2018-07-18 12:49:40 +0700

    Loops
    Lesson notes about loops

---
+++ HelloWorld/Loops.py
@@ -0,0 +1,66 @@
+# Loops
+#
+# Loops types:
+# - While loops
+# - Infinite loops
+# - While loops
+# - Nested loops
+#
+# WHILE loops
+# Syntax:
+# while expressions:
+#     statements
+# OR:
+# x = 0
+# while x < 10: print(x); x+=1
+# Example:
+# x = 0
+# while x < 10:
+#     print(x)
+#     x+=1
+
+# INIFINITE loops:
+# x = 0
+# while x < 10:
+#     print(x)
+
+# # ELSE WITH WHILE loops:
+# x = 0
+# while x < 10:
+#     print(x)
+#     x+=1
+# else:
+#     print("x >= 10")
+
+# NESTED WHILE loops:
+# Syntax:
+# while expression:
+#     while expression:
+#         statement(s)
+#     statement(S)
+# Example:
+# b = 0;
+# while b <= 10:
+#     x = 0
+#     print(b)
+#     while x <= 15:
+#         print(x)
+#         x+=1
+#     b+=1
+
+# FOR loops:
+# Syntax:
+# for iterating_var in sequence:
+#     statments(s)
+# Example:
+# for letter in 'python':
+#     print(letter)
+
+# NESTED FOR loops:
+# for i in range (2,20):
+#     for x in range(2,i):
+#         if i %  x == 0:
+#             print(i, "equals", x, " * ", i/x)
+#             break
+#     else:
+#         print(i, " is a prime number")
